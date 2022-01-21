c5cbf6f38b24a9cb1b99487be3aad4cb2be25a55
Sam <92101227+sdawley1@users.noreply.github.com>
2022-01-21 12:30:54 -0500

    Update job_submission.py

--- g09_command/job_submission.py
+++ g09_command/job_submission.py
@@ -3,17 +3,13 @@ from user_info import write_g09
 from ssh_client import ssh_conn
 from instructions import gaussian09
 
-# I'm just gonna put this here in case there's some error
-# so I don't have it run at the end of my last function
-os.system('stty echo')
-
 # Collecting user information
 user_info = write_g09()
 shFile, gjfFile = gaussian09(user_info)
 print("Files successfully created.\n")
 
 # Initializing parameters
-hostname, port = "login.marcc.jhu.edu", 22
+hostname, port = "ENTER HOSTNAME HERE", (ENTER INTEGER PORT HERE)
 username = user_info['email']
 
 # Connecting to SSH
